929b363daf69598f6374e115dd6937687926bd5d
student keshavjalkshatri <keshavjalkshatri@gmail.com>
2023-12-11 18:03:35 +0530

    new program area of square.c

---
+++ c program/basics/area of square.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+ int main(){
+        float side,area;
+        printf("enter the side of square : ");
+        scanf("%f",&side);
+        area = pow(side,2);
+        printf("area of the circle is %f",area);
+    }
