0b49b4bb570b54537a77c68038958b4655293398
student keshavjalkshatri <keshavjalkshatri@gmail.com>
2023-12-11 17:26:20 +0530

    new program sum_of_2no.c

---
+++ c program/basics/sum_of_2no.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+    int main(){
+        int a,b;
+        printf("enter number 1 : ");
+        scanf("%d",&a);
+        printf("enter number 2 : ");
+        scanf("%d",&b);
+        printf("sum of two no. is %d",a+b);
+        return 0;
+    }
