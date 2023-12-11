dbb5d8bb7aa79de12f8718db4523ca610a426051
student keshavjalkshatri <keshavjalkshatri@gmail.com>
2023-12-11 20:13:07 +0530

    new directory cpp program

---
+++ cpp program/basic/hello.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //prints  hello world in cpp 
+    cout <<"hello world" << endl;
+    cout << "i am a keshav jalkshatri";
+    return 0;
+}
