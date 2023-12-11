3f79f76ffcca10ea240eeb2dc92c4734b94b7472
student keshavjalkshatri <keshavjalkshatri@gmail.com>
2023-12-11 20:49:58 +0530

    new program logical,relational.c

---
+++ c program/basics/operations/Relational.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+    int main(){
+            int a,b; //declare two integer a and b.
+            printf("enter the value of a :\t");
+            scanf("%d",&a);
+            printf("enter the value of b :\t");
+            scanf("%d",&b); 
+            printf("a>b  : %d\n",a>b);
+            printf("a<b  : %d\n",a<b);
+            printf("a>=b : %d\n",a>=b);
+            printf("a<=b : %d\n",a<=b);
+            printf("a==b : %d\n",a==b);
+            printf("a!=b : %d\n",a!=b);
+            return 0;
+    }
---
+++ c program/basics/operations/logical.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+    int main(){
+            printf("0&&0 : %d\n",0&&0);
+            printf("0||0 : %d\n",0||0);
+            printf("!0   : %d\n",!0);
+            printf("0&&1 : %d\n",0&&1);
+            printf("0||1 : %d\n",0||1);
+            printf("!1   : %d\n",!1);
+            return 0;
+    }
