11ff57ccfe66b80cd9216dfe83f00105f978a5a4
student keshavjalkshatri <keshavjalkshatri@gmail.com>
2024-01-15 12:43:34 +0530

    commit

---
+++ c program/problem solving/puzzle001.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int first = 10;
+    int second = 20;
+    int third = 30;
+    {
+        int third = second - first;
+        printf("%d",third);
+    }
+    printf("\n%d",third);
+    return 0;
+}
